17f3527ed2b52c27ae95988a6628e357dc443f13
woodRed <>
2021-11-16 09:32:45 +0000

    replit connect github

---
+++ python/logging_ex.py
@@ -0,0 +1,29 @@
+import logging
+import mylib
+
+
+def main():
+  # 로그 생성
+  logger = logging.getLogger()
+
+  # 로그의 출력 기준 설정
+  logger.setLevel(logging.INFO)
+
+  # 로그 출력 형식
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+  # 로그 출력
+  stream_handler = logging.StreamHandler()
+  stream_handler.setFormatter(formatter)
+  logger.addHandler(stream_handler)
+
+  # 로그를 파일에 출력
+  file_handler = logging.FileHandler('my.log')
+  file_handler.setFormatter(formatter)
+  logger.addHandler(file_handler)
+  
+
+if __name__ == '__main__':
+  main()
+  mylib.do_something()
+  
---
+++ python/mylib.py
@@ -0,0 +1,5 @@
+import logging
+
+def do_something():
+  logging.info('Doing something.')
+  logging.warning('%s before you %s', 'Look', 'leap!')
